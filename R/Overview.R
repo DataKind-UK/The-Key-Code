846839a0cb8120530c5847db901a60a15f6e2daa
castrillo <laura.castrillo@possible.com>
2015-07-19 10:30:14 +0100

    Overview
    Trends in projects, active organisations

---
+++ R/Overview.R
@@ -0,0 +1,158 @@
+#R script for overview
+
+#Load packages
+  library(dplyr)    # summarize data
+  library(reshape2) # data manipulation
+  library(sqldf)   # write sql queries in R
+  library(stringr) # string operations (str_match)
+  library(ggplot2) # Graphs
+  library(scales)  # Format numbers in axes
+  library(RColorBrewer)  # package containing color palettes
+  myPalette <- brewer.pal(7,"Set2")
+
+#Set working directories
+  wd <- paste0(getwd(),"/")
+  input <- paste0(wd, "Data/")
+  output <- paste0(wd, "R Output/")
+  
+#Custom functions
+  renameColumn <- function(df, oldName, newName){
+    varnames <- names(df)
+    varnames[varnames == oldName] <- newName
+    names(df) <- varnames
+    return(df)
+  }
+
+#Load data
+  merged <- read.csv(paste0(input, "key_analysis_data_1700.csv"), stringsAsFactors=FALSE)
+  merged$year <- as.numeric(substr(merged$registered,1,4))
+  merged$month <- as.numeric(substr(merged$registered,6,7))
+  merged$graphMonth <- as.Date(ISOdate(merged$year, merged$month, 1))
+  head(merged$registered)
+  nrow(merged)
+  merged <- merged[which(merged$year >= 2004),]
+  nrow(merged)
+
+  #To search for fields by name use syntax below
+  names(merged)[grep("org", names(merged))]
+  
+#Overview graphs  
+#check group project years
+  summaryProjectYear <- merged %>%
+    group_by(year) %>%
+    summarise(projects=n())
+  summaryProjectYear$charity <- "key"
+  sum(summaryProjectYear$projects)
+  
+  graphData <- summaryProjectYear
+  ggplot(graphData, aes(x=factor(year), y=projects, color=charity, group=charity)) +
+    geom_point() +
+    geom_line(size=2) +
+    scale_color_manual(values=myPalette) +
+    scale_y_continuous(labels=comma_format()) +
+    expand_limits(y = 0) +
+    scale_x_discrete(name="") +
+    theme(legend.position="none") +
+    theme(text = element_text(size=15))
+
+#trend in active organisations over time  
+  prepOrgCount <- merged %>%
+    group_by(year, organisation_id_o, organisation_name) %>%
+    summarise(projects=n())    
+  head(prepOrgCount)
+  sum(prepOrgCount$projects)
+  
+  orgsYear <- prepOrgCount %>%
+    group_by(year) %>%
+    summarise(orgCount=length(unique(organisation_id_o)))
+  orgsYear$charity <- "key"
+  tail(orgsYear)
+  
+  graphData <- orgsYear
+  ggplot(graphData, aes(x=factor(year), y=orgCount, color=charity, group=charity)) +
+    geom_point() +
+    geom_line(size=2) +
+    scale_color_manual(values=myPalette[2]) +
+    scale_y_continuous(name="organisations with active projects", labels=comma_format()) +
+    expand_limits(y = 0) +
+    scale_x_discrete(name="") +
+    theme(legend.position="none") +
+    theme(text = element_text(size=15)) 
+  
+  #Organisations and projects over time
+  #head(summaryProjectYear)
+  #head(orgsYear)
+  summaryOrgsProjectYear <- sqldf("SELECT A.year, A.projects, B.orgCount as activeOrganisations
+                                  FROM summaryProjectYear A
+                                    INNER JOIN orgsYear B ON A.year = B.year")
+  summaryOrgsProjectYear
+
+  graphData <- melt(summaryOrgsProjectYear, c("year"))
+  ggplot(graphData, aes(x=factor(year), y=value, group=variable, color=variable)) +
+    geom_point() +
+    geom_line(size=1) +
+    scale_y_continuous(name="", labels=comma_format()) +
+    scale_x_discrete(name="") +
+    expand_limits(y = 0) +
+    scale_color_manual(values=myPalette) +
+    theme(text = element_text(size=15)) +
+    theme(legend.title=element_blank()) +
+    facet_wrap(~variable, scales="free", ncol=1)
+  
+  
+  #seasonality - monthly projects
+  #monthly projects registered
+  monthProject <- merged %>% 
+    group_by(year, month) %>%
+    summarise(projects=n())
+  
+  graphData <- monthProject
+  ggplot(graphData, aes(x=factor(month), y=projects, group=factor(year), color=factor(year))) +
+    geom_point() +
+    geom_line(size=1) +
+    scale_x_discrete(name="month") +
+    theme(legend.title=element_blank()) +
+    theme(text = element_text(size=15))
+  
+#Focus on recent data to understand top projects and facilitators
+  mergedRecent <- merged[which(merged$year >= 2014),]
+  
+  #Total number of organisations - 274
+  length(unique(mergedRecent$OrganisationID))
+  
+  #Organisations - Rank by activity
+  summaryOrgProject <- mergedRecent %>%
+    group_by(organisation_id_o, organisation_name) %>%
+    summarise(projects=n()) %>%
+    arrange(-projects) 
+  
+  summaryOrgProject$rankProjects <- as.numeric(row.names(summaryOrgProject))
+  summaryOrgProject$pctProjects <- summaryOrgProject$projects / sum(summaryOrgProject$projects) 
+  summaryOrgProject$cumPctProjects <- cumsum(summaryOrgProject$pctProjects)
+  summaryOrgProject$pctRank <- summaryOrgProject$rankProjects / max(summaryOrgProject$rankProjects)
+  summaryOrgProject$charity <- "key"
+  str(summaryOrgProject)
+  
+  head(summaryOrgProject)
+  
+  #Concentration - What % of projects do the top % of organisations account for?
+  graphData <- summaryOrgProject
+  ggplot(graphData, aes(x=pctRank, y=cumPctProjects, group=charity)) +
+    geom_point() +
+    scale_x_continuous(name="% organisations", labels=percent) +
+    scale_y_continuous(name="% projects", labels=percent) +
+    theme(text = element_text(size=15))
+  
+  
+  #top 15 organisations
+  graphData <- summaryOrgProject[which(summaryOrgProject$rankProjects <= 15),]
+  graphData <- transform(graphData, label=reorder(organisation_name, -rankProjects))
+  ggplot(graphData, aes(x=label, y=projects, fill=charity)) +
+    geom_bar(stat="identity") +
+    scale_fill_manual(values=myPalette[3]) +
+    scale_x_discrete(name="") +
+    theme(legend.position="none") +
+    theme(text = element_text(size=15)) +
+    coord_flip()
+  
+  
